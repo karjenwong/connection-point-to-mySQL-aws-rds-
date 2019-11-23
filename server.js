1027a5d1560922646a933678c98d26b69c28c39b
Karjen Wong <karjenwong@gmail.com>
2019-11-23 15:51:29 -0800

    tracking ignore files

--- server.js
+++ server.js
@@ -1,33 +1,15 @@
-require("dotenv").config();
-
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
 const cors = require("cors");
 const bodyParser = require("body-parser");
-var mysql = require("mysql");
-
-var connection = mysql.createConnection({
-  host: process.env.host,
-  user: process.env.user,
-  password: process.env.password,
-  port: process.env.port
-});
-
 app.use(bodyParser.json());
 app.use(cors());
-connection.connect();
-connection.query("USE students_db", function(error, results, fields) {
-  if (error) throw error;
-  console.log("connected");
-});
-app.get("/", function(req, res) {
-  connection.query("DESCRIBE students", function(error, results, fields) {
-    if (error) throw error;
-    res.send(results);
-    console.log(results);
-  });
-});
+
+app.get("/allinventory", (req, res) => {
+  res.json({ "response": data })
+})
+
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
