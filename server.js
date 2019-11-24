08d62e6064845c4298078628ac4f433472c554fd
Karjen Wong <karjenwong@gmail.com>
2019-11-23 16:00:08 -0800

    removed unncessary modules, connection td rds correctly and properly setup environmental settings

--- server.js
+++ server.js
@@ -1,15 +1,35 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = 8080;
 const cors = require("cors");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/allinventory", (req, res) => {
-  res.json({ "response": data })
-})
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+  host: process.env.host,
+  user: process.env.user,
+  password: process.env.password,
+  port: process.env.port
+});
 
+connection.connect();
+
+connection.query("USE students_db", function(error, results, fields) {
+  if (error) throw error;
+  console.log("Connection to amazon rds established.");
+});
+
+app.get("/", (req, res) => {
+  connection.query("DESCRIBE students", function(error, results, fields) {
+    if (error) throw error;
+    res.json(results)
+  });
+
+});
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
